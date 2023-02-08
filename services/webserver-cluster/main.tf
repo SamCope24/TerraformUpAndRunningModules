68dd0bb21721fd902429c44d1c79739fda9bdad0
Sam <samcope24@gmail.com>
2023-02-08 20:43:20 +0000

    added conditional auto-scaling in module

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -177,3 +177,29 @@ data "terraform_remote_state" "db" {
     region = "us-east-2"
   }
 }
+
+# scheduled action to increase ASG capacity in core business hours
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  count = var.enable_autoscaling ? 1 : 0
+  
+  scheduled_action_name = "${var.cluster_name}-scale-out-during-business-hours"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 10
+  recurrence = "0 9 * * *" # cron syntax - 9am every day
+
+  autoscaling_group_name = aws_autoscaling_group.example.name
+}
+
+# scheduled action to decrease ASG capacity outside of core business hours
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  count = var.enable_autoscaling ? 1 : 0
+
+  scheduled_action_name = "${var.cluster_name}-scale-in-at-night"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 2
+  recurrence = "0 17 * * *" # cron syntax - 5pm every day
+
+  autoscaling_group_name = aws_autoscaling_group.example.name
+}
